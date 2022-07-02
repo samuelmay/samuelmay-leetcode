8caccc52842ef124dbd927eebfabe21ab504994b
Samuel May <sam.m4y@gmail.com>
2022-07-02 21:56:53 +1000

    This works but it feels a bit monstrous. I don't really understand the part where I needed 'cloned()' and the pattern matching in 'compile()' is horrible.

--- Rust/0010_regex_matching.rs
+++ Rust/0010_regex_matching.rs
@@ -1,18 +1,70 @@
 fn compile(pattern: String) -> (usize, Box<dyn Fn(usize,char) -> usize>) {
 	let mut state_transitions : Vec<Box<dyn Fn(char) -> usize>> = Vec::new();
 	let mut current_state : usize = 0;
-	let mut stream = pattern.chars();
+	let mut stream = pattern.chars().peekable();
 	
 	while let Some(c) = stream.next() {
+		let peek_star = stream.peek();
 		let state = current_state;
-		let transition_function = move |i : char| {
-			if i == c {
-				return state + 1;
-			} else {
-				return 0
+		if let Some(star) = peek_star {
+			if *star == '*' {
+				// compile a multiple match function
+				// Consume the star metacharacter '*' and peek ahead again.
+				let _ = stream.next();
+				let peek_next_c = stream.peek().cloned();
+				if let Some(next_c) = peek_next_c {
+					// we have a future character that causes us to transition out of the greedy
+					// matching state.
+
+					// consume the character that ends the greedyness
+					let _ = stream.next();
+					let test_char = next_c;
+
+					if c == '.' {
+						// wildcard matches anything
+						state_transitions.push(Box::new(move |i : char| {
+							if i == test_char {
+								return state + 1;
+							} else {
+								return state;
+							}
+						}));
+					} else {
+						// the next character needs to be matched
+						// specifically
+						state_transitions.push(Box::new(move |i : char| {
+							if i == c {
+								return state;
+							} else if i == test_char {
+								return state + 1;
+							} else {
+								return 0;
+							}
+						}));
+					}
+				} else {
+					// there is a greedy star at the end of the regexp
+					// This is a trivial case that always matches
+					state_transitions.push(Box::new(move |_i : char| state + 1));
+				}
+				current_state += 1;
+				continue;
 			}
-		};
-		state_transitions.push(Box::new(transition_function));
+		}
+
+		// No greedy star modifier
+		if c == '.' {
+			// wildcard! match anything
+			state_transitions.push(Box::new(move |_i : char| state + 1));
+		} else {
+			state_transitions.push(Box::new(move |i : char| {
+				if i == c {
+					return state + 1;
+				} else {
+					return 0
+				}
+			}));
+		}
 		current_state += 1;
 	}
 	
@@ -25,8 +77,10 @@ fn compile(pattern: String) -> (usize, Box<dyn Fn(usize,char) -> usize>) {
 }
 fn is_match(s: String, p: String) -> bool {
 	let (end_state,delta) = compile(p);
+	let mut s_terminated : Vec<char> = s.chars().collect();
+	s_terminated.push('\0');
 	let mut q = 0;
-	for i in s.chars() {
+	for i in s_terminated {
 		q = delta(q,i);
 		if q == end_state {
 			return true;
@@ -36,8 +90,8 @@ fn is_match(s: String, p: String) -> bool {
 }
 
 fn main() {
-	let examples = vec!["quick brown faox","triple aaa gaming","aa battery"];
-	let pattern = "brown";
+	let examples = vec!["driveme","sata drive","attttt","bbbba"];
+	let pattern = "a*dr";
 
 	for example in examples {
 		let p = String::from(pattern);
