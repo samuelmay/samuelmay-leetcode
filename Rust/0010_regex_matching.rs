cda7e97a7c27b98be01bbb6de5281c21b848817b
Samuel May <sam.m4y@gmail.com>
2022-07-03 23:19:09 +1000

    Hooray it works. Had to do some serious borrowing to get the recursive depth first search to work.

--- Rust/0010_regex_matching.rs
+++ Rust/0010_regex_matching.rs
@@ -50,6 +50,7 @@ impl FiniteAutomata {
 	fn add_state(&mut self) -> StateIndex {
 		let index = self.states.len();
 		self.states.push(AutomataState { first_transition: None });
+		//println!("Adding state {}",index);
 		return index;
 	}
 	
@@ -62,6 +63,7 @@ impl FiniteAutomata {
 			next_outgoing_transition: state_data.first_transition
 		});
 		state_data.first_transition = Some(transition_num);
+		//println!("Adding transition {} -> {} -> {}",source, symbol, target);
 	}
 	
 	fn transition_states(&self, source: StateIndex, match_symbol: char) -> NextStates {
@@ -83,26 +85,38 @@ fn compile(pattern: String) -> FiniteAutomata {
 		transitions: Vec::new()
 	};
 	let mut current_state : StateIndex;
-	let mut stream = pattern.chars();
+	let mut stream = pattern.chars().peekable();
 	
 	// start state always exists
 	automata.initial_state = automata.add_state();
 	current_state = automata.initial_state;
-	println!("Adding initial state {}", automata.initial_state);
 	
 	while let Some(symbol) = stream.next() {
-		match symbol {
-			'*' => {
+		let next_symbol = stream.peek();
+		match (symbol,next_symbol) {
+			(_,Some('*')) => {
 				// Kleene star
-				// Add 'Kleene closure' to previous state
-				
-				
-			}
-			_ => {
+				// Add 'Kleene closure'
+				let state_inner_1 = automata.add_state();
+				let state_inner_2 = automata.add_state();
+				let skip_state = automata.add_state();
+
+				// add the skip link
+				automata.add_transition(current_state, skip_state, '*');
+				// add the link to the inner, symbol-matching transition 
+				automata.add_transition(current_state, state_inner_1, '*');
+				// add the symbol-matching transition
+				automata.add_transition(state_inner_1, state_inner_2, symbol);
+				// add the loopback link
+				automata.add_transition(state_inner_2, state_inner_1, '*');
+				// add the exit link
+				automata.add_transition(state_inner_2, skip_state, '*');
+				current_state = skip_state;
+			},
+			('*',_) => continue, // skip the Kleene star as it was already processed when we peeked at it
+			(_,_) => {
 				let next_state = automata.add_state();
-				println!("Adding state {}",next_state);
 				automata.add_transition(current_state, next_state, symbol);
-				println!("Adding transition {} -> {} -> {}",current_state, symbol, next_state);
 				current_state = next_state;
 			}
 		}
@@ -115,27 +129,55 @@ fn compile(pattern: String) -> FiniteAutomata {
 fn is_match(s: String, p: String) -> bool {
 	let automata : FiniteAutomata = compile(p);
 
-	let mut current_state : StateIndex;
-	current_state = automata.initial_state;
-	println!("Current state is {}", current_state);
+	let mut current_states : Vec<StateIndex> = Vec::new();
+	let mut visited : Vec<StateIndex> = Vec::new();
+	search_next_states(&automata, automata.initial_state, &mut current_states, &mut visited);
+	println!("Current states is {:?}", current_states);
 	
 	for i in s.chars() {
-		for next_state in automata.transition_states(current_state, i) {
-			current_state = next_state;
+		let previous_states = current_states.clone();
+		current_states.clear();
+		for state in previous_states {
+			for next_state in automata.transition_states(state, i) {
+				visited.clear();
+				search_next_states(&automata, next_state, &mut current_states, &mut visited);
+			}
 		}
-		println!("Current state is {}", current_state);
+		println!("Current states is {:?}", current_states);
+		if current_states.len() == 0 {
+			return false;
+		}
 	}
 	println!("Testing against final state {}...", automata.final_state);
-	return current_state == automata.final_state;
+	return current_states.contains(&automata.final_state);
+}
+
+fn search_next_states(
+		automata : &FiniteAutomata,
+		start : StateIndex,
+		next_states : &mut Vec<StateIndex>,
+		visited : &mut Vec<StateIndex>
+	) {
+	let epsilon_transitions : Vec<StateIndex> = automata.transition_states(start,'*').collect();
+	if epsilon_transitions.len() == 0 {
+		next_states.push(start);
+	} else {
+		for state in epsilon_transitions {
+			if !visited.contains(&state) {
+				visited.push(state);
+				search_next_states(automata,state,next_states,visited);
+			}
+		}
+	}
 }
 
 fn main() {
-	let examples = vec!["aaa","","abc","axc"];
-	let pattern = "a.c";
+	let examples = vec!["ab"];
+	let pattern = ".*";
 
 	for example in examples {
 		let p = String::from(pattern);
 		let result = is_match(example.to_string(),p);
-		println!("{} -> {}", example, result);
+		println!("    = {} -> {}", example, result);
 	}
 }
