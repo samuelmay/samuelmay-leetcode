c0637989b6622b421e3afdf54f359874ecd56b5a
Samuel May <sam.m4y@gmail.com>
2022-07-03 16:01:00 +1000

    Did some research on the wildcard problem and discovered a proper explanation of how to create a regex finite automata from the original academic concepts. Also found an article on creating graph data structures in Rust. With that as a guide, we are a *lot* closer to a concise and understandable solution.

--- Rust/0010_regex_matching.rs
+++ Rust/0010_regex_matching.rs
@@ -1,105 +1,133 @@
-fn compile(pattern: String) -> (usize, Box<dyn Fn(usize,char) -> usize>) {
-	let mut state_transitions : Vec<Box<dyn Fn(char) -> usize>> = Vec::new();
-	let mut current_state : usize = 0;
-	let mut stream = pattern.chars().peekable();
-	
-	while let Some(c) = stream.next() {
-		let peek_star = stream.peek();
-		let state = current_state;
-		if let Some(star) = peek_star {
-			if *star == '*' {
-				// compile a multiple match function
-				// Consume the star metacharacter '*' and peek ahead again.
-				let _ = stream.next();
-				let peek_next_c = stream.peek().cloned();
-				if let Some(next_c) = peek_next_c {
-					// we have a future character that causes us to transition out of the greedy
-					// matching state.
+type StateIndex = usize;
+type TransitionIndex = usize;
 
-					// consume the character that ends the greedyness
-					let _ = stream.next();
-					let test_char = next_c;
+struct FiniteAutomata {
+	initial_state: StateIndex,
+	final_state: StateIndex,
+	states: Vec<AutomataState>,
+	transitions: Vec<AutomataTransition>,
+}
 
-					if c == '.' {
-						// wildcard matches anything
-						state_transitions.push(Box::new(move |i : char| {
-							if i == test_char {
-								return state + 1;
-							} else {
-								return state;
-							}
-						}));
-					} else {
-						// the next character needs to be matched
-						// specifically
-						state_transitions.push(Box::new(move |i : char| {
-							if i == c {
-								return state;
-							} else if i == test_char {
-								return state + 1;
-							} else {
-								return 0;
-							}
-						}));
-					}
-				} else {
-					// there is a greedy star at the end of the regexp
-					// This is a trivial case that always matches
-					state_transitions.push(Box::new(move |_i : char| state + 1));
-				}
-				current_state += 1;
-				continue;
+struct AutomataState {
+	first_transition: Option<TransitionIndex>,
+}
+
+struct AutomataTransition {
+	match_symbol: char,
+	target: StateIndex,
+	next_outgoing_transition: Option<TransitionIndex>
+}
+
+struct NextStates<'a> {
+	automata : &'a FiniteAutomata,
+	symbol: char,
+	current_transition_index: Option<TransitionIndex>
+}
+
+impl<'a> Iterator for NextStates<'a> {
+	type Item = TransitionIndex;
+
+	fn next(&mut self) -> Option<TransitionIndex> {
+		loop {
+			if self.current_transition_index == None {
+				return None;
+			}
+			let transition_num = self.current_transition_index.unwrap();
+			let transition = &self.automata.transitions[transition_num];
+			self.current_transition_index = transition.next_outgoing_transition;
+			if transition.match_symbol == self.symbol {
+				return Some(transition.target);
 			}
 		}
-
-		// No greedy star modifier
-		if c == '.' {
-			// wildcard! match anything
-			state_transitions.push(Box::new(move |_i : char| state + 1));
-		} else {
-			state_transitions.push(Box::new(move |i : char| {
-				if i == c {
-					return state + 1;
-				} else {
-					return 0
-				}
-			}));
-		}
-		current_state += 1;
 	}
-	
-		
-	let final_state = current_state;
-	let delta_function = Box::new(move |q : usize, i : char| {
-		return state_transitions[q](i);
-	});
-
-	return (final_state,delta_function);
 }
-fn is_match(s: String, p: String) -> bool {
-	// we want to match the exact string length but I had already
-	// gone the whole hog for partial matching so this is a quick hack
-	// to fix it. We are already adding a '\0' null terminator to every
-	// string given.
-	let mut s_terminated = s.clone();
-	s_terminated.push('\0'); 
-	let mut p_terminated = p.clone();
-	p_terminated.push('\0');
+
+impl FiniteAutomata {
+	fn add_state(&mut self) -> StateIndex {
+		let index = self.states.len();
+		self.states.push(AutomataState { first_transition: None });
+		return index;
+	}
 	
-	let (end_state,delta) = compile(p_terminated);
-	let mut q = 0;
-	for i in s_terminated.chars() {
-		q = delta(q,i);
-		if q == end_state {
-			return true;
+	fn add_transition(&mut self, source: StateIndex, target: StateIndex, symbol: char) {
+		let transition_num = self.transitions.len();
+		let state_data = &mut self.states[source];
+		self.transitions.push(AutomataTransition {
+			match_symbol: symbol,
+			target: target,
+			next_outgoing_transition: state_data.first_transition
+		});
+		state_data.first_transition = Some(transition_num);
+	}
+	
+	fn transition_states(&self, source: StateIndex, match_symbol: char) -> NextStates {
+		let first_transition = self.states[source].first_transition;
+		return NextStates {
+			symbol: match_symbol,
+			automata: self,
+			current_transition_index: first_transition
 		}
 	}
-	return false;
+
+}
+
+fn compile(pattern: String) -> FiniteAutomata {
+	let mut automata = FiniteAutomata {
+		initial_state: 0,
+		final_state: 0,
+		states: Vec::new(),
+		transitions: Vec::new()
+	};
+	let mut current_state : StateIndex;
+	let mut stream = pattern.chars();
+	
+	// start state always exists
+	automata.initial_state = automata.add_state();
+	current_state = automata.initial_state;
+	println!("Adding initial state {}", automata.initial_state);
+	
+	while let Some(symbol) = stream.next() {
+		match symbol {
+			'*' => {
+				// Kleene star
+				// Add 'Kleene closure' to previous state
+				
+				
+			}
+			_ => {
+				let next_state = automata.add_state();
+				println!("Adding state {}",next_state);
+				automata.add_transition(current_state, next_state, symbol);
+				println!("Adding transition {} -> {} -> {}",current_state, symbol, next_state);
+				current_state = next_state;
+			}
+		}
+	}
+	automata.final_state = current_state;
+	println!("Final state is {}", automata.final_state);
+	return automata;
+}
+
+fn is_match(s: String, p: String) -> bool {
+	let automata : FiniteAutomata = compile(p);
+
+	let mut current_state : StateIndex;
+	current_state = automata.initial_state;
+	println!("Current state is {}", current_state);
+	
+	for i in s.chars() {
+		for next_state in automata.transition_states(current_state, i) {
+			current_state = next_state;
+		}
+		println!("Current state is {}", current_state);
+	}
+	println!("Testing against final state {}...", automata.final_state);
+	return current_state == automata.final_state;
 }
 
 fn main() {
-	let examples = vec!["baab","bbc","","bb"];
-	let pattern = "ba*b";
+	let examples = vec!["aaa","","abc"];
+	let pattern = "abc";
 
 	for example in examples {
 		let p = String::from(pattern);
