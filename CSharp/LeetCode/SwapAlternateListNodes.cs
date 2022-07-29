59524ebd43156610ea8f0a7623bb0246e5a3f1f6
Samuel May <sam.m4y@gmail.com>
2022-07-29 18:33:19 +1000

    Problem 24 another list node one. In C#

---
+++ CSharp/LeetCode/SwapAlternateListNodes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SwapAlternateListNodesSolution
+    {
+        public ListNode SwapPairs(ListNode head) {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            ListNode alternate = head.next;
+            ListNode tail = SwapPairs(alternate.next);
+            alternate.next = head;
+            head.next = tail;
+            return alternate;
+        }
+    }
+}
---
+++ CSharp/Tests/SwapListNodesTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using LeetCode;
+
+namespace Tests
+{
+    public class SwapListNodesTests
+    {
+        private SwapAlternateListNodesSolution solution;
+        public SwapListNodesTests()
+        {
+            solution = new SwapAlternateListNodesSolution();
+        }
+
+        [Test]
+        public void LeetcodeExample()
+        {
+            ListNode list1 = ListNode.FromArray(new int[] { 1, 2, 3, 4 });
+            ListNode result = solution.SwapPairs(list1);
+            string output = result.ToString();
+            Assert.AreEqual(output, "(2,1,4,3)");
+
+        }
+
+        [Test]
+        public void OddLengthExample()
+        {
+            ListNode list1 = ListNode.FromArray(new int[] { 5,6,7,8,9 });
+            ListNode result = solution.SwapPairs(list1);
+            string output = result.ToString();
+            Assert.AreEqual(output, "(6,5,8,7,9)");
+        }
+
+        [Test]
+        public void NullInput()
+        {
+            ListNode? result = solution.SwapPairs(null);
+            Assert.IsNull(result);
+
+            ListNode oneElement = ListNode.FromArray(new int[] { 1 } );
+            result = solution.SwapPairs(oneElement);
+            Assert.AreEqual(result.val, 1);
+            Assert.IsNull(result.next);
+        }
+        
+    }
+}
