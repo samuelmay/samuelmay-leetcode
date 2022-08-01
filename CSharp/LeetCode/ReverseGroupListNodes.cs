8b7ffeede6e460f3ce2ba111b55590e95095a4be
Samuel May <sam.m4y@gmail.com>
2022-08-01 21:41:12 +1000

    Problem 25, reverse nodes in k group. I didn't realize C# actually uses a Stack collection type that's different to List (which doesn't have push/pop). TIL.

---
+++ CSharp/LeetCode/ReverseGroupListNodes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ReverseNodesKGroup
+    {
+        public ListNode ReverseKGroup(ListNode head, int k) {
+
+            ListNode groupHead = head;
+            ListNode previousGroupHead = null;
+            ListNode nextGroupHead = null;
+            ListNode result = null;
+            Stack<ListNode> stack = new Stack<ListNode>(k);
+            while (groupHead != null)
+            {
+                ListNode current = groupHead;
+                // Count 'k' nodes, push them onto the stack
+                int i = 0;
+                for (i = 0; i < k && current != null; i++)
+                {
+                    stack.Push(current);
+                    current = current.next;
+                }
+                // if there aren't 'k' nodes left in the list, we're done.
+                // Return
+                if (i < k)
+                {
+                    return result ?? groupHead;
+                }
+                // This is the furthest we go in this loop. Save a reference to
+                // the start of the next group
+                nextGroupHead = current;
+                
+                // start reversing the list, using the stack
+                current = stack.Pop();
+                // if there is no previous group, this is the first group we've reversed,
+                // so save a reference to the new start of this list - it will
+                // be the final return value
+                if (previousGroupHead == null)
+                {
+                    result = current;
+                    previousGroupHead = groupHead;
+                }
+                else
+                {
+                    previousGroupHead.next = current;
+                }
+
+                // reverse the group
+                while (stack.Count > 0)
+                {
+                    ListNode previous = stack.Pop();
+                    current.next = previous;
+                    current = previous;
+                }
+                current.next = groupHead;
+
+                // create the link to the next group
+                groupHead.next = nextGroupHead;
+                // assign the next group as the current group, for the next loop
+                previousGroupHead = groupHead;
+                groupHead = nextGroupHead;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CSharp/Tests/ReverseGroupListNodesTest.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using LeetCode;
+
+namespace Tests
+{
+    public class ReverseGroupListNodesTest
+    {
+        private ReverseNodesKGroup solution;
+        public ReverseGroupListNodesTest()
+        {
+            solution = new ReverseNodesKGroup();
+        }
+
+        [Test]
+        public void LeetcodeExample1()
+        {
+            ListNode list1 = ListNode.FromArray(new int[] { 1, 2, 3, 4, 5 });
+            ListNode result = solution.ReverseKGroup(list1,2);
+            string output = result.ToString();
+            Assert.AreEqual("(2,1,4,3,5)",output);
+
+        }
+        [Test]
+        public void LeetcodeExample2()
+        {
+            ListNode list1 = ListNode.FromArray(new int[] { 1, 2, 3, 4, 5 });
+            ListNode result = solution.ReverseKGroup(list1,3);
+            string output = result.ToString();
+            Assert.AreEqual("(3,2,1,4,5)",output);
+
+        }
+        [Test]
+        public void DivisibleLengthExample()
+        {
+            ListNode list1 = ListNode.FromArray(new int[] { 1,2,3,4,5,6,7,8,9 });
+            ListNode result = solution.ReverseKGroup(list1,3);
+            string output = result.ToString();
+            Assert.AreEqual("(3,2,1,6,5,4,9,8,7)",output);
+        }
+
+        [Test]
+        public void EntireLengthExample()
+        {
+            ListNode list1 = ListNode.FromArray(new int[] { 1,2,3,4,5,6 });
+            ListNode result = solution.ReverseKGroup(list1,6);
+            string output = result.ToString();
+            Assert.AreEqual("(6,5,4,3,2,1)",output);
+        }
+        [Test]
+        public void NullInput()
+        {
+            ListNode? result = solution.ReverseKGroup(null,5);
+            Assert.IsNull(result);
+
+            ListNode oneElement = ListNode.FromArray(new int[] { 1 } );
+            result = solution.ReverseKGroup(oneElement,1);
+            Assert.AreEqual(1, result.val);
+            Assert.IsNull(result.next);
+
+            result = solution.ReverseKGroup(oneElement,77);
+            Assert.AreEqual(1, result.val);
+            Assert.IsNull(result.next);
+
+        }
+        
+    }
+}
