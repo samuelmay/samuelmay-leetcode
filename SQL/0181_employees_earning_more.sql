5063b90c2cfd720f260500830792cc685cfc1bf9
Samuel May <sam.m4y@gmail.com>
2022-08-03 14:57:40 +1000

    Couple more easy SQL ones. Good to remember the 'HAVING' clause you can add on to a 'GROUP BY', which is apparently more performant than a subquery

--- SQL/0181_employees_earning_more.sql
+++ SQL/0181_employees_earning_more.sql
@@ -4,3 +4,5 @@ insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', 
 insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
 insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None');
 insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None');
+
+select e.name as Employee from Employee e inner join Employee m on e.managerId = m.id where e.salary > m.salary;
---
+++ SQL/0182_duplicate_emails.sql
@@ -0,0 +1,7 @@
+Create table If Not Exists Person (id int, email varchar(255));
+Truncate table Person;
+insert into Person (id, email) values ('1', 'a@b.com');
+insert into Person (id, email) values ('2', 'c@d.com');
+insert into Person (id, email) values ('3', 'a@b.com');
+
+select email as Email from (select count(email) as dupes, email from Person group by email) tmp where tmp.dupes > 1;
