0446cc5e8ca985c8e0bda0ed3061feba61a5d725
Samuel May <sam@samuelmay.net>
2022-10-21 09:48:19 +1100

    Formatting

--- SQL/0184_department_highest_salary.sql
+++ SQL/0184_department_highest_salary.sql
@@ -1,1 +1,8 @@
-select d.name, e.Employee, e.Salary from Department d inner join (select name as Employee, salary as Salary, departmentId, rank() over (partition by departmentId order by salary desc) as SalaryRank from Employee) e on d.id = e.departmentId where SalaryRank = 1;
+select d.name, e.Employee, e.Salary
+ from Department d inner join
+ (select name as Employee,
+	salary as Salary,
+	departmentId,
+	rank() over (partition by departmentId order by salary desc) as SalaryRank
+    from Employee) e
+ on d.id = e.departmentId where SalaryRank = 1;
